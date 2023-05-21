d6f4178861a4d0cc98c92a98b8df6be00dacc9b4
redaready <redaready.dev@gmail.com>
2023-05-21 08:38:47 +0000

    lesson01

---
+++ src/main/java/com/innoxjp/course/lesson01/App.java
@@ -0,0 +1,22 @@
+/*----------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *---------------------------------------------------------------------------------------*/
+
+package com.innoxjp.course.lesson01;
+
+
+import java.sql.Time;
+import java.time.Duration;
+import java.util.Date;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Remote World!");
+    }
+
+    void 跳瑜伽(Time 时间, Duration duration){
+        时间.compareTo(new Date(2023,1,20));
+
+    }
+}
---
+++ src/main/java/com/innoxjp/course/lesson01/Bus.java
@@ -0,0 +1,7 @@
+package com.innoxjp.course.lesson01;
+
+public class Bus {
+    int a = 20;
+    
+    
+}
