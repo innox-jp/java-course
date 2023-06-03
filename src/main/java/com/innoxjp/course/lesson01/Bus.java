6af1427578c815438acc742204819ce77e0a6001
redaready <redaready.dev@gmail.com>
2023-06-03 07:55:25 +0000

    test

--- src/main/java/com/innoxjp/course/lesson01/Bus.java
+++ src/main/java/com/innoxjp/course/lesson01/Bus.java
@@ -3,15 +3,31 @@ package com.innoxjp.course.lesson01;
 class Bus {
     //primary type
     short height = 1;
-    int a = 20;
-    long changDu = 3;
+    Short sheight = 1;
 
+    int a = 20;
+    
+    Integer ia = 20;
+    ia.
+
+
+    long changDu = 3;
+    Long lchangDu = 3;
+    
+    
     float speed = (float) 1.0;
+    float fl = 0.3;
+    Float Fl = 0.3;
     String name = "中央bus线";
     boolean huaiMeiHuai = false;
-    byte meiYouYong = 'b';
+    Boolean bhuaBoolean = true;
+    byte meiYouYong = 'j';
+    Byte 字节 = "1";
     char title = 'a';
+    Char ctitle = 'a';
     double speedDouble = 1.0;
+    Double dspeeDouble = 2.0;
+
     int seatNbl = 30;
 
 
