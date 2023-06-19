fcc12394cd6f4d540f2c6fe910dfa35048f9c8ba
redaready <redaready.dev@gmail.com>
2023-06-19 01:23:10 +0000

    第4课， integer 类， String类

--- src/main/java/com/innoxjp/course/lesson01/人类.java
+++ src/main/java/com/innoxjp/course/lesson01/人类.java
@@ -1,6 +1,7 @@
 package com.innoxjp.course.lesson01;
 class 物体{
     public Integer money;
+    public static String TYPE = "物体";
 }
 
 
@@ -34,11 +35,27 @@ public class 人类 extends 动物 {
     public void 吃饭(){}
     public void 睡觉(){}
     public void 做瑜伽(){}
+    public static void main(String[] 参数) {
+
+        Integer 丽华的数 = new Integer("0");
+        //Integer 小刘的数 = new Integer("小刘");
+        String 丽华的名字 = "张丽华";
+        String 王的名字 = "张亮";
+        String 小时 = "";
+        System.out.println(丽华的名字.length());
+        System.out.println(王的名字.length());
+        System.out.println(String.format("好漂亮的 %s",王的名字));
+        System.out.println(小时.isEmpty());
+        System.out.println(丽华的名字.charAt(2));
+        System.out.println(王的名字.startsWith("张"));
+        
+    }
 }
 
 class LiHua extends 人类 implements 打高尔夫{
 
     public void 打高尔夫() {
+        Integer 分数 = new Integer("0");
     }
 
 }
